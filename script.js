bf1f766de3e83a546aa5f997d05b6d54fce6c0cf
Yudhi <baskyudh@gmail.com>
2025-02-12 14:20:08 +0700

    Add logic for click on number buttons - Split-second highlight on click - Main display text and num1 value updated on each click - To figure out how to remove leading zero from default main disp innerText value

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 "use strict";
 
-// const d = document.querySelector('');
-// const a = document.querySelector('');
-// const b = document.querySelector('');
-// const c = document.querySelector('');
+const mainDispText = document.querySelector(".main-dsp");
+const numBtnNodes = document.querySelectorAll(".num-btn");
+// const b = document.querySelector("");
+// const c = document.querySelector("");
 
-const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
-
-const num1 = 0;
-const operator = "";
-const num2 = 0;
+let num1 = "";
+let operator = "";
+let num2 = "";
 
 const operate = (num1, operator, num2) => {
+  const add = (a, b) => a + b;
+  const subtract = (a, b) => a - b;
+  const multiply = (a, b) => a * b;
+  const divide = (a, b) => a / b;
+
   switch (operator) {
     case "+":
       return add(num1, num2);
@@ -28,16 +28,35 @@ const operate = (num1, operator, num2) => {
     case "/":
       return divide(num1, num2);
       break;
-      
   }
 };
 
-console.log(add(1, 2));
-console.log(subtract(8, 23));
-console.log(multiply(21, 12));
-console.log(divide(213, 24));
+numBtnNodes.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.target.style.backgroundColor = "#FF43A7";
+    setTimeout(() => {
+      e.target.style.backgroundColor = "#f72798";
+    }, 100);
+    // mainDispText.innerText = "";
+    if (btn.innerText === "." && mainDispText.innerText.includes(".")) {
+      alert("No multiple dots allowed");
+    } else {
+      mainDispText.innerText += btn.innerText;
+      num1 = Number(mainDispText.innerText);
+      console.log(num1);
+      console.log(typeof num1);
+    }
+  });
+});
 
-console.log(operate(1,"+",2));
-console.log(operate(1,"-",2));
-console.log(operate(1,"*",2));
-console.log(operate(1,"/",2));
+
+
+// console.log(add(1, 2));
+// console.log(subtract(8, 23));
+// console.log(multiply(21, 12));
+// console.log(divide(213, 24));
+
+// console.log(operate(1,"+",2));
+// console.log(operate(1,"-",2));
+// console.log(operate(1,"*",2));
+// console.log(operate(1,"/",2));
