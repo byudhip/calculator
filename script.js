5e10269a89a71e9a8c7d7a0f663b4fca587364e7
Yudhi <baskyudh@gmail.com>
2025-02-13 10:25:35 +0700

    Major update - Change mainDisp queryselector due to change in HTML file - Operate now convert num1 & num2 to number at the start of function - Add prevention for operation above 1e+15 to simplify the calculator - Add alert & return 0 for operation above 1e+15 - Fix error when only one of num is a float - Add validation to numBtn if mainDispText.innerText is undefined - Add prevention to numBtn press if any num is already 15 chars long - Refined console.log messages - To remove leading zero from showing up on certain operations

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const mainDispText = document.querySelector(".main-dsp");
+const mainDispText = document.querySelector(".main-disp");
 const numBtnNodes = document.querySelectorAll(".num-btn");
 const opBtn = document.querySelectorAll(".op-btn");
 const miscBtn = document.querySelectorAll(".misc-btn");
@@ -12,6 +12,8 @@ let num2 = "";
 let num1toggle = true;
 
 const operate = (num1, operator, num2) => {
+  num1 = Number(num1);
+  num2 = Number(num2);
   const add = (a, b) => a + b;
   const subtract = (a, b) => a - b;
   const multiply = (a, b) => a * b;
@@ -19,28 +21,122 @@ const operate = (num1, operator, num2) => {
 
   switch (operator) {
     case "+":
-      return Number.isInteger(add(num1, num2))
-        ? add(num1, num2)
-        : add(num1, num2).toFixed(2);
+      if (
+        Number.isInteger(num1) &&
+        Number.isInteger(num2) &&
+        add(num1, num2) >= 1e+15
+      ) {
+        alert("go buy scientific calculator");
+        return 0;
+      } else if (
+        Number.isInteger(num1) &&
+        Number.isInteger(num2) &&
+        add(num1, num2) < 1e+15
+      ) {
+        return add(num1, num2);
+      } else if (
+        (!Number.isInteger(num1) ||
+        !Number.isInteger(num2)) &&
+        add(num1, num2) >= 1e+15
+      ) {
+        alert("go buy scientific calculator");
+        return 0;
+      } else if (
+        (!Number.isInteger(num1) ||
+        !Number.isInteger(num2)) &&
+        add(num1, num2) < 1e+15
+      ) {
+        return add(num1, num2).toFixed(2);
+      }
       break;
     case "-":
-      return Number.isInteger(subtract(num1, num2))
-        ? subtract(num1, num2)
-        : subtract(num1, num2).toFixed(2);
+      if (
+        Number.isInteger(num1) &&
+        Number.isInteger(num2 && subtract(num1, num2)) >= 1e+15
+      ) {
+        alert("go buy scientific calculator");
+        return 0;
+      } else if (
+        Number.isInteger(num1) &&
+        Number.isInteger(num2) &&
+        subtract(num1, num2) < 1e+15
+      ) {
+        return subtract(num1, num2);
+      } else if (
+        (!Number.isInteger(num1) ||
+        !Number.isInteger(num2)) &&
+        subtract(num1, num2) >= 1e+15
+      ) {
+        alert("go buy scientific calculator");
+        return 0;
+      } else if (
+        (!Number.isInteger(num1) ||
+        !Number.isInteger(num2)) &&
+        subtract(num1, num2) < 1e+15
+      ) {
+        return subtract(num1, num2).toFixed(2);
+      }
       break;
     case "*":
-      return Number.isInteger(multiply(num1, num2))
-        ? multiply(num1, num2)
-        : multiply(num1, num2).toFixed(2);
+      if (
+        Number.isInteger(num1) &&
+        Number.isInteger(num2) &&
+        multiply(num1, num2) >= 1e+15
+      ) {
+        alert("go buy scientific calculator");
+        return 0;
+      } else if (
+        Number.isInteger(num1) &&
+        Number.isInteger(num2) &&
+        multiply(num1, num2) < 1e+15
+      ) {
+        return multiply(num1, num2);
+      } else if (
+        (!Number.isInteger(num1) ||
+        !Number.isInteger(num2)) &&
+        multiply(num1, num2) >= 1e+15
+      ) {
+        alert("go buy scientific calculator");
+        return 0;
+      } else if (
+        (!Number.isInteger(num1) ||
+        !Number.isInteger(num2)) &&
+        multiply(num1, num2) < 1e+15
+      ) {
+        return multiply(num1, num2).toFixed(2);
+      }
       break;
     case "/":
-      return Number.isInteger(divide(num1, num2))
-        ? divide(num1, num2)
-        : divide(num1, num2).toFixed(2);
-      break;
+      if (
+        Number.isInteger(num1) &&
+        Number.isInteger(num2) &&
+        divide(num1, num2) >= 1e+15
+      ) {
+        alert("go buy scientific calculator");
+        return 0;
+      } else if (
+        Number.isInteger(num1) &&
+        Number.isInteger(num2) &&
+        divide(num1, num2) < 1e+15
+      ) {
+        return divide(num1, num2);
+      } else if (
+        (!Number.isInteger(num1) ||
+        !Number.isInteger(num2)) &&
+        divide(num1, num2) >= 1e+15
+      ) {
+        alert("go buy scientific calculator");
+        return 0;
+      } else if (
+        (!Number.isInteger(num1) ||
+        !Number.isInteger(num2)) &&
+        divide(num1, num2) < 1e+15
+      ) {
+        return divide(num1, num2).toFixed(2);
+        break;
+      }
   }
 };
-
 numBtnNodes.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     e.target.style.backgroundColor = "#FF43A7";
@@ -50,26 +146,26 @@ numBtnNodes.forEach((btn) => {
     if (btn.innerText === "." && mainDispText.innerText.includes(".")) {
       alert("No multiple dots allowed");
     } else if (
-      (mainDispText.innerText === "0" || mainDispText.innerText === "LEMAW") &&
+      (mainDispText.innerText === "0" || mainDispText.innerText === "LEMAW" || mainDispText.innerText === "undefined") &&
       !operator &&
       num1toggle
     ) {
       mainDispText.innerText = btn.innerText === "." ? "0" : "";
       mainDispText.innerText += btn.innerText;
       num1 = mainDispText.innerText;
-      console.log(`This is num1 value ${num1},`, typeof num1);
-    } else if (num1.length < 12 && !operator) {
+      console.log(`This is num1 value: ${num1 || "empty string"},`, typeof num1);
+    } else if (num1.length < 15 && !operator) {
       mainDispText.innerText += btn.innerText;
       num1 = mainDispText.innerText;
-      console.log(`This is num1 value ${num1},`, typeof num1);
-    } else if (num1 && operator) {
+      console.log(`This is num1 value: ${num1 || "empty string"},`, typeof num1);
+    } else if (num1 && operator && num2.length < 16) {
       num1toggle = false;
       console.log(`num1 toggle is ${num1toggle}`);
       mainDispText.innerText = num2;
       mainDispText.innerText += btn.innerText;
       num2 = mainDispText.innerText;
-      console.log(`This is num2 value ${num2},`, typeof num2);
-    } else if (num1.length === 12 || num2.length === 12) {
+      console.log(`This is num2 value: ${num2 || "empty string"},`, typeof num2);
+    } else if (num1.length === 15 || num2.length === 15) {
       alert("Number is outside operating range");
     }
   });
@@ -86,9 +182,11 @@ opBtn.forEach((btn) => {
     } else if (btn.innerText !== "=") {
       operator += btn.innerText;
       num1toggle = false;
-      console.log(`This is the value of operator: ${operator} and toggle: ${num1toggle}`);
+      console.log(
+        `This is the value of operator: ${operator} and toggle: ${num1toggle}`
+      );
     } else if (btn.innerText === "=") {
-      if (num2 === 0 && operator === "/") {
+      if (num2 === "0" && operator === "/") {
         mainDispText.innerText = "LEMAW";
         num1toggle = true;
         num1 = "";
@@ -100,7 +198,9 @@ opBtn.forEach((btn) => {
         num1toggle = false;
         num2 = "";
         operator = "";
-        console.log(`num1 toggle is ${num1toggle}, num1 is ${num1} num2 is ${num2}`);
+        console.log(
+          `num1 toggle is ${num1toggle}, num1 is ${num1 || "empty string"} num2 is ${num2 || "empty string"}`
+        );
       }
     }
   });
@@ -119,56 +219,58 @@ miscBtn.forEach((btn) => {
       num1 = "";
       num2 = "";
       operator = "";
+      console.log(
+        `AC button pressed, all variables returned to their original state`
+      );
     } else if (btn.innerText === "C") {
       if (
         mainDispText.innerText === "undefined" ||
-        mainDispText.innerText === "LEMAW"
+        mainDispText.innerText === "LEMAW" ||
+        mainDispText.innerText === "0" ||
+        mainDispText.innerText === "Infinity"
       ) {
         return;
+        console.log(`C button pressed, did nothing`);
       }
       mainDispText.innerText =
-        mainDispText.innerText.length <= 2 && mainDispText.innerText.startsWith("-")
+        (mainDispText.innerText.length <= 2 &&
+        mainDispText.innerText.startsWith("-")) || mainDispText.innerText.length === 1
           ? "0"
           : mainDispText.innerText.slice(0, mainDispText.innerText.length - 1);
-      console.log(`this is toggle value ${num1toggle}`);
+      console.log(`C button pressed, this is toggle value ${num1toggle}`);
       if (num1toggle) {
         num1 = mainDispText.innerText;
-        console.log(`This is num1 value ${num1},`, typeof num1);
-        console.log(`This is num2 value ${num2},`, typeof num2);
+        console.log(`This is num1 value: ${num1 || "empty string"},`, typeof num1);
+        console.log(`This is num2 value: ${num2 || "empty string"},`, typeof num2);
       } else if (!num1toggle) {
         num2 = mainDispText.innerText === "0" ? "" : mainDispText.innerText;
-        console.log(`This is num1 value ${num1},`, typeof num1);
-        console.log(`This is num2 value ${num2},`, typeof num2);
+        console.log(`This is num1 value: ${num1 || "empty string"},`, typeof num1);
+        console.log(`This is num2 value: ${num2 || "empty string"},`, typeof num2);
       }
     } else if (btn.innerText === "+/-") {
-      if (
-        !mainDispText.innerText.startsWith("-") //&&
-        // mainDispText.innerText.length >= 1 &&
-        // !mainDispText.innerText.charAt(0) === "0"
-      ) {
+      if (!mainDispText.innerText.startsWith("-") && mainDispText.innerText !== "0") {
         mainDispText.innerText = "-" + mainDispText.innerText;
         if (num1toggle) {
           num1 = mainDispText.innerText;
-          console.log(`This is num1 value ${num1},`, typeof num1);
-          console.log(`This is num2 value ${num2},`, typeof num2);
+          console.log(`This is num1 value: ${num1 || "empty string"},`, typeof num1);
+        console.log(`This is num2 value: ${num2 || "empty string"},`, typeof num2);
         } else if (!num1toggle) {
           num2 = mainDispText.innerText;
-          console.log(`This is num1 value ${num1},`, typeof num1);
-          console.log(`This is num2 value ${num2},`, typeof num2);
+          console.log(`This is num1 value: ${num1 || "empty string"},`, typeof num1);
+        console.log(`This is num2 value: ${num2 || "empty string"},`, typeof num2);
         }
       } else if (mainDispText.innerText.startsWith("-")) {
         mainDispText.innerText = mainDispText.innerText.slice(1);
         if (num1toggle) {
           num1 = mainDispText.innerText;
-          console.log(`This is num1 value ${num1},`, typeof num1);
-          console.log(`This is num2 value ${num2},`, typeof num2);
+          console.log(`This is num1 value: ${num1 || "empty string"},`, typeof num1);
+        console.log(`This is num2 value: ${num2 || "empty string"},`, typeof num2);
         } else if (!num1toggle) {
           num2 = mainDispText.innerText;
-          console.log(`This is num1 value ${num1},`, typeof num1);
-          console.log(`This is num2 value ${num2},`, typeof num2);
+          console.log(`This is num1 value: ${num1 || "empty string"},`, typeof num1);
+        console.log(`This is num2 value: ${num2 || "empty string"},`, typeof num2);
         }
       }
     }
   });
 });
-
